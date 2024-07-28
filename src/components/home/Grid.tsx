5a1cb22b92eaf65715d65ba9d3a1acb5c3b66f69
facundoacostayl <facundoacostayl@outlook.com>
2024-07-28 19:37:42 -0300

    add Grid component

---
+++ src/components/home/Grid.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { Brand } from "../../types/Brand";
+
+interface GridProps {
+  data: Brand[];
+}
+
+export const Grid = ({ data }: GridProps) => {
+  return (
+    <div className="grid items-center max-w-4xl grid-cols-2 gap-4 mx-auto mt-12 md:mt-20 md:grid-cols-4">
+      {data?.map(({ id, name, imageUrl }) => (
+        <Link to={`/products/brand/${id}`}>
+          <div className="bg-white hover:bg-gray-100 h-20 flex shadow-lg items-center justify-center ease-in duration-100">
+            {imageUrl ? (
+              <img
+                className="object-contain w-full h-20 mx-auto"
+                src={imageUrl}
+                alt={name}
+              />
+            ) : (
+              <p className="font-semibold text-xl text-gray-600">{name}</p>
+            )}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import brandService from "../services/Brands";
 import { useQuery } from "react-query";
+import { Grid } from "../components/home/Grid";
 
 export const Home = () => {
   const { data } = useQuery("brands", () => brandService.findBrands());
@@ -15,23 +16,7 @@ export const Home = () => {
           <p className="font-light">Powered by Facundo Acosta :)</p>
         </div>
 
-        <div className="grid items-center max-w-4xl grid-cols-2 gap-4 mx-auto mt-12 md:mt-20 md:grid-cols-4">
-          {data?.map(({ id, name, imageUrl }) => (
-            <Link to={`/products/brand/${id}`}>
-              <div className="bg-white hover:bg-gray-100 h-20 flex shadow-lg items-center justify-center ease-in duration-100">
-                {imageUrl ? (
-                  <img
-                    className="object-contain w-full h-20 mx-auto"
-                    src={imageUrl}
-                    alt={name}
-                  />
-                ) : (
-                  <p className="font-semibold text-xl text-gray-600">{name}</p>
-                )}
-              </div>
-            </Link>
-          ))}
-        </div>
+        <Grid data={data!}></Grid>
 
         <div className="flex items-center justify-center mt-10 space-x-3 md:hidden">
           <div className="w-2.5 h-2.5 rounded-full bg-blue-600 block"></div>
