e91bea58f998540252a1bc31693dde09e8d81e40
facundoacostayl <facundoacostayl@outlook.com>
2024-07-28 06:54:54 -0300

    update Home main ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,122 @@
 export const Home = () => {
   return (
-    <div>
-      <p className="text-4xl">Products</p>
-    </div>
+    <section className="py-10 relative bg-white sm:py-16 lg:py-24 lg:pt-36">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 text-center  ">
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="mb-2 text-2xl font-light text-black sm:text-4xl sm:leading-tight">
+            Update your product's prices in one place.{" "}
+          </h2>
+          <p className="font-light">Powered by Proviamo Almacén Gourmet.</p>
+        </div>
+
+        <div className=" grid items-center max-w-4xl grid-cols-2 gap-4 mx-auto mt-12 md:mt-20 md:grid-cols-4  ">
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-6 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-1.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-2.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-3.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full mx-auto h-7"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-4.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-5.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-6.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-7.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-8.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-9.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full mx-auto h-7"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-10.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-11.png"
+              alt=""
+            />
+          </div>
+
+          <div className="bg-white h-12 flex shadow-lg items-center justify-center">
+            <img
+              className="object-contain w-full h-8 mx-auto"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-12.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center mt-10 space-x-3 md:hidden">
+          <div className="w-2.5 h-2.5 rounded-full bg-blue-600 block"></div>
+          <div className="w-2.5 h-2.5 rounded-full bg-gray-300 block"></div>
+          <div className="w-2.5 h-2.5 rounded-full bg-gray-300 block"></div>
+        </div>
+
+        <p className="mt-10 text-base text-center text-black md:mt-20 p-6 py-3 border w-72 border-black rounded-full mx-auto">
+          ver todos los productos
+        </p>
+      </div>
+    </section>
   );
 };
